01db116fa8d5a57d8735ab33efde1b8ffb302732
PranshuPandey90 <138874525+PranshuPandey90@users.noreply.github.com>
2023-07-14 17:50:07 +0530

    Add files via upload

---
+++ src/common/common.js
@@ -0,0 +1,33 @@
+export const portfolio = [
+    {
+        "status": 1,
+        "data": {
+            "web_developer": "I'm Web Developer",
+            "detail":"I have 1.7 years of experience working as a Frontend Developer, during which I have developed a strong proficiency in modern web technologies, including Html,Css, Javascript, Typescript, jQuery and framework such as Angular and Angular JS.I am well-versed in responsive design principles,cross-browser compatibility and mobile-first development practice.I also have experience with a version control system like git and have a good understanding of agile development methodologies.",
+            "Key": "Skills",
+            "skills": ["Angular","HTML5","CSS3","JavaScript","TypeScript","Restfull Api","Rxjs","Ngrx","JQuey","Git","Postman"],
+            "projects":[
+                {
+                   "title":"PayMaster (Angular 8/12, Angular Material, TypeScript,Bootstrap ,CSS)",
+                   "description":"Integrated end to end journey for Paymaster architecture.Where user can create rules for the following insurer and insurance journey like motor,cv,health,life and many more to calculate the payout for partners and insurer as well from the UI."
+                },
+                {
+                    "title":"Partner Portal ((Angular 8/12, Angular Material, TypeScript,Bootstrap ,CSS))",
+                    "description":"Integrated end to end journey for Partner Portal.Thats include various partners journey like veriﬁcation document, bank document.UI contain complete history of partners like number of insurance sold,total number of business done, their payments status and all.Integrated with helpdesk to raise question, quotation of insurance and other features as well." 
+                },
+                {
+                    "title":"OPS Portal (HTML,JavaScript, Jquery,Bootstrap ,CSS)",
+                    "description":"Integrated end to end journey for Operation Portal.Thats include various Insurance journeys like Motor, CV, Health, Life with separate dashboards. UI contains a complete journey form for the particular Insurance sold by our partners.Features like permission access, payment status, policy status etc."
+                 },
+                 {
+                     "title":"Travel Insurance (HTML,JavaScript, Jquery,Bootstrap ,CSS)",
+                     "description":"Integrated end to end journey for Travel Insurance.Integrated end to end journey for Operation Portal.Thats include various Insurance journeys like Motor, CV, Health, Life with separate dashboards. UI contains a complete journey form for the particular Insurance sold by our partners.Features like permission access, payment status, policy status etc."
+                 },
+            ]
+
+        },
+        "message": "SuccessFull"
+
+    }
+
+]
